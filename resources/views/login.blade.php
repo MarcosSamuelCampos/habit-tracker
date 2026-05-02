caefc631ee63b0410ebfb6b47316376edf070066
MarcosSamuelCampos <kampusam@gmail.com>
2026-05-02 16:50:35 -0300

    feat(Auth): add funcionalidade de Login e sessao usuario

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+<main class="py-10">
+     <h1>Faça o Login</h1>
+</main>
+
+   
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 use League\Uri\Http;
 
-
+// SITE
 Route::get('/', [SiteController::class, 'index']);
+
+// LOGIN
+Route::get('/login',[LoginController::class, 'index']);
